bad5debf27f7baeac51ebe32ecac7b2cf2a6f5e3
Mohammad Kaab <mohammad.kaab@gmail.com>
2020-07-05 19:34:35 +0300

    Change the config key

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (config('app.production')) {
+        if (config('app.env')) {
             $url->forceScheme('https');
         }
     }
