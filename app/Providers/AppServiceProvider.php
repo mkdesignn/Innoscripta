28d9248ace02e19c3d52d61a6e559d2d5cda92e1
Mohammad Kaab <mohammad.kaab@gmail.com>
2020-07-05 19:37:06 +0300

    Brain is not helping, Just fixing the https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (config('app.env')) {
+        if (config('app.env') === 'production') {
             $url->forceScheme('https');
         }
     }
